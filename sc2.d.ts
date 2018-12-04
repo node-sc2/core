6b28efc4a32262a1d57fd30a962822210e058723
Jared Kantrowitz <jared.kantrowitz@gmail.com>
2018-12-04 17:35:53 -0500

    fixed typing inference for agent blueprint

--- sc2.d.ts
+++ sc2.d.ts
@@ -1,6 +1,6 @@
 /// <reference path="./interfaces.d.ts" />
 
-export function createAgent(blueprint?: AgentObject): Agent;
+export function createAgent(blueprint?: EventReader<AgentObject>): Agent;
 export function createEngine(opts?: EngineOptions): Engine;
 export function createSystem(sys: EventReader<SystemObject>, opts?: SystemOptions): EventReader<System>;
 export function createPlayer(settings, agent?: Agent): any;
