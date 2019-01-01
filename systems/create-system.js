987aa7c07d167fcc22b3da78420019fc9fdbd312
Jared Kantrowitz <jared.kantrowitz@gmail.com>
2019-01-01 17:08:18 -0500

    system entity enhancements and fixes
    systems now default to type 'agent' if not specified
    .pause() and .unpause() for internal system manipulation
    fixed stepIncrement not actually affecting anything, should work now
    allow for 'late setup' of systems after game is running
    expose system from wrapper via escape hatch

--- systems/create-system.js
+++ systems/create-system.js
@@ -3,8 +3,11 @@
 const debug = require('debug')('sc2:debug:system');
 const debugSilly = require('debug')('sc2:silly:system');
 const pascalCase = require('pascal-case');
+const unitPlugin = require('./unit-plugin');
 const { builderPlugin } = require('./builder-plugin');
 const { NodeSC2Error, GameEndedError } = require('../engine/errors');
+
+const NOOP = () => {};
 
 /**
  * This digests a SystemObject, making it consumable by a bot to be run by
@@ -19,8 +22,10 @@ function createSystem(sys, opts = {}) {
         ...opts,
     };
 
-    const { stepIncrement = 8 } = options;
+    const { stepIncrement = sys.type === 'agent' ? 8 : 1 } = options;
 
+    const pauseSym = Symbol('pause');
+    
     /**
      * create internal system, initialize state and behavior -
      * all future system convention enhancements should go here
@@ -29,7 +34,13 @@ function createSystem(sys, opts = {}) {
      * @type {EventReader<System>}
      */
     const system = {
+        type: 'agent',
         ...sys,
+        [pauseSym]: false,
+        // @ts-ignore symbol indexes still not supported...
+        pause() { this[pauseSym] = true; },
+        // @ts-ignore symbol indexes still not supported...
+        unpause() { this[pauseSym] = false; },
         setup: sys.setup || (() => {}),
         state: options.state || {},
         setState: function setState(newState) {
@@ -61,16 +72,20 @@ function createSystem(sys, opts = {}) {
      */
     async function systemWrapper(world) {
         const { frame, events } = world.resources.get();
-
         const eventsWithHandlers = events.read(system.readerId).filter(event => system[`on${pascalCase(event.name)}`]);
         
+        // @ts-ignore
+        if (system[pauseSym]) {
+            return system.onStepPaused ? system.onStepPaused(world) : NOOP;
+        }
+
         const handlerPromises = eventsWithHandlers.length > 0 ?
             eventsWithHandlers.map((event) => {
                 if (event.name === 'step') {
                     _setState({ currentFrame: frame.getGameLoop() });
 
                     /* skip this system if not enough frames have passed since it last ran */
-                    if (system.type === 'agent' && _state.lastRan && _state.currentFrame - _state.lastRan < _state.stepIncrement) {
+                    if ((system.type === 'agent') && _state.lastRan && _state.currentFrame - _state.lastRan <= _state.stepIncrement) {
                         debugSilly(`skipping system ${system.name}: current loop frame: ${_state.currentFrame}, last ran: ${_state.lastRan}`);
                         return;
                     }
@@ -108,16 +123,26 @@ function createSystem(sys, opts = {}) {
         return Promise.all(handlerPromises);
     }
 
-    systemWrapper.setup = function(world) {
+    systemWrapper.setup = function(world, isLateSetup = false) {
         const { events } = world.resources.get();
         system.readerId = events.createReader(system.type);
         debugSilly(`readerId ${system.readerId} registered for initialized system ${systemWrapper.name}`);
 
         if (system.type === 'build' && system.buildOrder) {
             builderPlugin(system);
+        } else if (system.type === 'unit') {
+            unitPlugin(system);
         }
 
-        return system.setup ? system.setup(world) : system.readerId;
+        if (isLateSetup) {
+            if (system.setup) {
+                system.setup(world);
+            }
+
+            return system.onGameStart ? system.onGameStart(world) : system.readerId;
+        } else {
+            return system.setup ? system.setup(world) : system.readerId;
+        }
     };
 
     Object.defineProperty(systemWrapper, 'name', {
@@ -125,6 +150,7 @@ function createSystem(sys, opts = {}) {
         configurable: true,
     });
 
+    systemWrapper._system = system;
     return systemWrapper;
 }
 
