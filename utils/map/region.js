a19bc7aaabbca7c7b2cd84cd9782a99147578e5b
Jared Kantrowitz <jared.kantrowitz@gmail.com>
2019-01-01 16:56:32 -0500

    region utility fixes / additions
    new `frontOfGrid` useful for basically any list of points

--- utils/map/region.js
+++ utils/map/region.js
@@ -1,5 +1,6 @@
 'use strict';
 
+const debugDebug = require('debug')('sc2:debug:RegionUtil');
 let natGraph;
 
 /**
@@ -9,7 +10,18 @@ let natGraph;
  */
 function frontOfNatural(map) {
     const natural = map.getNatural();
-    const enemyNat = map.getEnemyNatural();
+
+    if (!natural) {
+        debugDebug('Current map has no definable natural, can not calculate');
+        return [];
+    }
+
+    let enemyNat = map.getEnemyNatural();
+
+    if (!enemyNat) {
+        debugDebug('We do not know where our enemy nat is (yet?), defaulting to their main');
+        enemyNat = map.getEnemyMain();
+    }
 
     // natural area, sorted by distance from the enemy natural
     natGraph = natGraph || natural.areas.areaFill
@@ -17,7 +29,18 @@ function frontOfNatural(map) {
         .sort((a, b) => a.d - b.d);
 
     // slice off the front.. so uh, it's the front
-    return natGraph.slice(0, Math.floor(natGraph.length / 5));
+    return natGraph.slice(0, Math.floor(natGraph.length / 3));
 }
 
-module.exports = { frontOfNatural };
+function frontOfGrid({ resources }, grid) {
+    const { map } = resources.get();
+    const mapCenter = map.getCenter(true);
+
+    const frontGrid = grid
+        .map(point => ({ ...point, distance: map.path(point, mapCenter).length} ))
+        .sort((a, b) => a.distance - b.distance);
+
+    return frontGrid.slice(0, Math.floor(frontGrid.length / 3));
+}
+
+module.exports = { frontOfNatural, frontOfGrid };
