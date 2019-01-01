d8793507479449949f949d75f39f8d7d117119a7
Jared Kantrowitz <jared.kantrowitz@gmail.com>
2019-01-01 17:26:30 -0500

    constant fixes for consistency
    added UnitTypeId, AbilityId, and UpgradeId to the main export
    these are the reverses, to get the string representation of the ids
    from the ids (for stuff like labels / debugging)
    added WarpUnitAbility which maps a warp unit to its training ability id.

--- constants/index.js
+++ constants/index.js
@@ -8,12 +8,40 @@ const groups = require('./groups');
 const UnitType = require('./unit-type');
 const Upgrade = require('./upgrade');
 
+const UnitTypeId = Object.entries(UnitType).reduce((UnitTypeId, [unitType, unitTypeId]) => {
+    UnitTypeId[unitTypeId] = unitType;
+    return UnitTypeId;
+}, {});
+
+const AbilityId = Object.entries(Ability).reduce((AbilityId, [ability, abilityId]) => {
+    AbilityId[abilityId] = ability;
+    return AbilityId;
+}, {});
+
+const UpgradeId = Object.entries(Upgrade).reduce((UpgradeId, [upgrade, upgradeId]) => {
+    UpgradeId[upgradeId] = upgrade;
+    return UpgradeId;
+}, {});
+
+const WarpUnitAbility = {
+    [UnitType.ZEALOT]: Ability.TRAINWARP_ZEALOT,
+    [UnitType.STALKER]: Ability.TRAINWARP_STALKER,
+    [UnitType.SENTRY]: Ability.TRAINWARP_SENTRY,
+    [UnitType.ADEPT]: Ability.TRAINWARP_ADEPT,
+    [UnitType.HIGHTEMPLAR]: Ability.TRAINWARP_HIGHTEMPLAR,
+    [UnitType.DARKTEMPLAR]: Ability.TRAINWARP_DARKTEMPLAR,
+};
+
 module.exports = {
     Ability,
+    AbilityId,
     AbilitiesByUnit,
     Color,
     enums,
     groups,
     UnitType,
+    UnitTypeId,
     Upgrade,
+    UpgradeId,
+    WarpUnitAbility,
 };
