3e1fe3f802611a073aa7aa8f847ca5ff9e97848d
Jared Kantrowitz <jared.kantrowitz@gmail.com>
2019-01-08 11:11:46 -0500

    build type systems will now derp less on pylons

--- systems/builder-plugin.js
+++ systems/builder-plugin.js
@@ -276,24 +276,39 @@ function builderPlugin(system) {
             const [main, natural] = map.getExpansions();
             const mainMineralLine = main.areas.mineralLine;
 
-            const pylonsNearProduction = units.getById(PYLON)
-                .filter(u => u.buildProgress >= 1)
-                .filter(pylon => distance(pylon.pos, main.townhallPosition) < 50);
+            let placements;
+            if (task.id === PYLON) {
+                placements = [...main.areas.placementGrid, ...natural.areas.placementGrid]
+                    .filter((point) => {
+                        return (
+                            (distance(natural.townhallPosition, point) > 4.5) &&
+                            (mainMineralLine.every(mlp => distance(mlp, point) > 1.5)) &&
+                            (natural.areas.hull.every(hp => distance(hp, point) > 3)) &&
+                            (units.getStructures({ alliance: Alliance.SELF })
+                                .map(u => u.pos)
+                                .every(eb => distance(eb, point) > 3))
+                        );
+                    });
+            } else {
+                const pylonsNearProduction = units.getById(PYLON)
+                    .filter(u => u.buildProgress >= 1)
+                    .filter(pylon => distance(pylon.pos, main.townhallPosition) < 50);
 
-            if (pylonsNearProduction.length <= 0) return BuildResult.CANNOT_SATISFY;
+                if (pylonsNearProduction.length <= 0) return BuildResult.CANNOT_SATISFY;
 
-            const placements = [...main.areas.placementGrid, ...natural.areas.placementGrid]
-                .filter((point) => {
-                    return (
-                        (distance(natural.townhallPosition, point) > 4.5) &&
-                        (pylonsNearProduction.some(p => distance(p.pos, point) < 6.5)) &&
-                        (mainMineralLine.every(mlp => distance(mlp, point) > 1.5)) &&
-                        (natural.areas.hull.every(hp => distance(hp, point) > 2)) &&
-                        (units.getStructures({ alliance: Alliance.SELF })
-                            .map(u => u.pos)
-                            .every(eb => distance(eb, point) > 3))
-                    );
-                });
+                placements = [...main.areas.placementGrid, ...natural.areas.placementGrid]
+                    .filter((point) => {
+                        return (
+                            (distance(natural.townhallPosition, point) > 4.5) &&
+                            (pylonsNearProduction.some(p => distance(p.pos, point) < 6.5)) &&
+                            (mainMineralLine.every(mlp => distance(mlp, point) > 1.5)) &&
+                            (natural.areas.hull.every(hp => distance(hp, point) > 2)) &&
+                            (units.getStructures({ alliance: Alliance.SELF })
+                                .map(u => u.pos)
+                                .every(eb => distance(eb, point) > 3))
+                        );
+                    });
+            }
 
             if (placements.length <= 0) return BuildResult.CANNOT_SATISFY;
 
